91b2d7d93051a1b70264305b56a347604b80ac78
전병길 <jeonbg@kakao.com>
2023-02-14 21:32:27 +0900

    feat(pwa): theme color 수정

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -119,7 +119,7 @@ const config = {
           {
             tagName: "meta",
             name: "theme-color",
-            content: "rgb(37, 194, 160)",
+            content: "#242526",
           },
         ],
       },
